c981bee12964bdae55aaf1ce66c96296304be513
Anthony Farncombe <anthony.farncombe@redpack.co.uk>
2019-08-27 09:25:31 +0100

    Create Node.js app with Typescript and Prettier

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send('Hello');
+});
+
+app.listen(8000, () => console.log('Server running on port 8000'));
