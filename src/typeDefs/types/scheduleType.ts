54e8b3e84e7a8b9428c20d72bcc8ac398181aadd
Anthony Farncombe <anthony.farncombe@redpack.co.uk>
2020-08-17 21:11:26 +0100

    Fix schedules GraphQL

--- src/typeDefs/types/scheduleType.ts
+++ src/typeDefs/types/scheduleType.ts
@@ -3,13 +3,10 @@ import { gql } from "apollo-server-express";
 export const scheduleType = gql`
   type Schedule {
     id: ID!
-    name: String!
     dayOfWeek: String!
     timeOfMeeting: String!
-    warmupTime: String!
-    temperature: Float
-    frequency: Int!
+    frequency: String!
     startDate: String
-    overrideDay: Boolean
+    overrideDay: Boolean!
   }
 `;
