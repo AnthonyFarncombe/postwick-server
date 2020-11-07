9d96780935a765e47c1e27b04eea00bff6ebe004
Anthony Farncombe <anthony.farncombe@redpack.co.uk>
2020-11-07 20:05:14 +0000

    Remove GraphQL

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import http from "http";
 import express from "express";
 import bodyParser from "body-parser";
-import { ApolloServer } from "apollo-server-express";
 import cors from "cors";
 import chalk from "chalk";
 
@@ -9,34 +8,12 @@ import routes from "./routes";
 
 import socketModule from "./socket";
 
-import { getUserFromRequest, UserContext } from "./auth";
-
-import typeDefs from "./typeDefs";
-import resolvers from "./resolvers";
-
-export type ApolloContext = { ip: string | undefined; user: UserContext | undefined };
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }): Promise<ApolloContext> => {
-    let user: UserContext | undefined;
-    try {
-      user = await getUserFromRequest(req);
-    } catch (err) {}
-    const clientIpAddress = (req.headers["x-real-ip"] as string) || req.connection.remoteAddress || "";
-    return { ip: clientIpAddress, user };
-  },
-});
-
 const app = express();
 
 const httpServer = http.createServer(app);
 
 app.use(cors());
 app.use(bodyParser.json());
-
-server.applyMiddleware({ app });
 
 app.use("/api", routes);
 
@@ -49,6 +26,4 @@ try {
   port = 3000;
 }
 
-httpServer.listen(port, () =>
-  console.log(chalk.green(`Server running on http://localhost:${port}${server.graphqlPath}`)),
-);
+httpServer.listen(port, () => console.log(chalk.green(`Server running on http://localhost:${port}`)));
--- src/typeDefs/types/authType.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from "apollo-server-express";
-
-export const authType = gql`
-  type AuthData {
-    userId: ID!
-    token: String!
-    tokenExpiration: Int!
-  }
-`;
