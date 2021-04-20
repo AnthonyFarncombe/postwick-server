26aacf7454f5b95e402408da5f0b1cf4528e27f0
Anthony Farncombe <anthony.farncombe@redpack.co.uk>
2021-04-20 21:59:45 +0100

    Improved alarm notifications

--- src/alerts.ts
+++ src/alerts.ts
@@ -1,11 +1,16 @@
 import path from "path";
 import { storeEvents, Variable } from "./store";
 import { sendMail } from "./email";
+import Car from "./models/car";
 import VariableLog from "./models/variableLog";
-import { VisitType } from "./models/visit";
+import Visit, { VisitType } from "./models/visit";
 import User from "./models/user";
 import Mail from "nodemailer/lib/mailer";
 import { sendSMS } from "./aws";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
 
 let varsToLog: string[] = [];
 let enableLogging = false;
@@ -28,31 +33,92 @@ async function logEventsToDb(variable: Variable): Promise<void> {
 
 async function alarmAlerts(variable: Variable): Promise<void> {
   try {
+    // Alert cleared so no action
     if (!variable.value) return;
 
+    // Check if alarm event
     const match = /^([a-z]+)AlarmTriggered$/.exec(variable.name);
     if (!match) return;
 
+    // Look for a suspect
+    let suspect:
+      | { timeFromNow: string; imagePath?: string; plateText?: string; driverName?: string; vehicleType?: string }
+      | undefined;
+
+    // If it is the security alarm then check if someone has recently arrived
+    if (variable.name === "securityAlarmTriggered") {
+      // Look for any cars arriving within the last 30 minutes
+      const startDate = new Date();
+      startDate.setMinutes(startDate.getMinutes() - 30);
+
+      const visit = await Visit.findOne({
+        timestamp: { $gt: startDate },
+        imageNameOrig: { $exists: true },
+      })
+        .sort({ timestamp: -1 })
+        .exec();
+
+      if (visit) {
+        suspect = {
+          timeFromNow: dayjs(visit.timestamp).fromNow(true),
+          imagePath: visit.imageNameOrig,
+          plateText: visit.plateText,
+        };
+
+        // Check if the vehicle is known
+        if (suspect.plateText) {
+          const car = await Car.findOne({ plateText: visit.plateText }).exec();
+          if (car) {
+            suspect.driverName = car.name;
+            suspect.vehicleType = car.vehicleType;
+          }
+        }
+      }
+    }
+
+    // Get all users subscribed to alarm notifications
     const users = await User.find({ notifications: "alarm" });
     if (!users) return;
 
+    // Get mobile numbers and email addresses
     const smsUsers = users.filter(u => u.mobile);
     const emailUsers = users.filter(u => u.email);
 
+    // Generate SMS message body
+    let message = match[1][0].toUpperCase() + match[1].slice(1) + " Alarm Triggered - Postwick";
+    if (suspect && suspect.driverName) {
+      message += `, possibly caused by ${suspect.driverName} who arrived ${suspect.timeFromNow} earlier`;
+    } else if (suspect && suspect.plateText) {
+      message += `, possibly caused by the driver of ${suspect.plateText.substr(0, 4) +
+        suspect.plateText.substr(4, 3)} who arrived ${suspect.timeFromNow} earlier`;
+    }
+
+    // Send all SMS messages
     smsUsers.forEach(
       async u =>
         await sendSMS({
           phoneNumber: u.mobile,
-          message: match[1][0].toUpperCase() + match[1].slice(1) + " Alarm Triggered - Postwick",
+          message,
         }),
     );
 
+    // Add email attachments
+    const attachments: Mail.Attachment[] = [];
+    if (suspect && suspect.imagePath && process.env.CCTV_ARCHIVE) {
+      attachments.push({
+        filename: suspect.imagePath,
+        path: path.resolve(process.env.CCTV_ARCHIVE, suspect.imagePath),
+      });
+    }
+
+    // Send all emails
     if (emailUsers) {
       await sendMail({
         to: emailUsers.map(u => ({ name: `${u.firstName} ${u.lastName}`, address: u.email })),
         template: "alarm",
         subject: match[1][0].toUpperCase() + match[1].slice(1) + " Alarm Triggered - Postwick",
-        context: { type: match[1] },
+        context: { type: match[1], suspect },
+        attachments,
       });
     }
   } catch (err) {
